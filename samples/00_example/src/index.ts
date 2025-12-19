5b3c7476b6f4d13b0962404b13f7fb409c5617b3
basarat <basaratali@gmail.com>
2025-12-19 12:12:08 +1100

    wrap

--- samples/00_example/src/index.ts
+++ samples/00_example/src/index.ts
@@ -1,3 +1,7 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
-console.log(chalk.blue('Hello world!'));
+const error = chalk.bold.red;
+const warning = chalk.hex('#FFA500');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
